c2df73259d9aceee830e36e6c00135075ad2351b
satyam4853 <vaishnavsatyam48532gmail.com>
2021-08-31 12:53:37 +0530

    [satyam] Add: AddessBook wh No Duplicates.

--- Addressbook_Using-Lambda/Addressbook_Using-Lambda/Program.cs
+++ Addressbook_Using-Lambda/Addressbook_Using-Lambda/Program.cs
@@ -6,7 +6,38 @@ namespace Addressbook_Using_Lambda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*******************Welcome To Address Book Using Lambda Problem *********************");
+            Console.WriteLine("**************************Welcome To Address Book Using Lambda Problem ****************************");
+
+            AddressBookDetails addressbookdetails = new AddressBookDetails();
+
+            while (true)
+            {
+
+                
+                Console.WriteLine("1.Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        addressbookdetails.AddMember();
+                        break;
+                    case 2:
+                        addressbookdetails.ViewContacts();
+                        break;
+                    case 3:
+                        addressbookdetails.EditDetails();
+                        break;
+                    case 4:
+                        addressbookdetails.DeleteDetails();
+                        break;
+                    case 5:
+                        // to exit from main method
+                        Console.WriteLine("Exited");
+                        return;
+
+                }
+
+            }
         }
     }
 }
