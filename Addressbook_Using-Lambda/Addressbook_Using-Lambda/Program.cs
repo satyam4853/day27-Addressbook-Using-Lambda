74176e1cc6d573c9d48fcbcf363111afa3c6df96
satyam4853 <vaishnavsatyam48532gmail.com>
2021-08-31 12:45:54 +0530

    [satyam] Add : Welcome Message

---
+++ Addressbook_Using-Lambda/Addressbook_Using-Lambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Addressbook_Using_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************Welcome To Address Book Using Lambda Problem *********************");
+        }
+    }
+}
